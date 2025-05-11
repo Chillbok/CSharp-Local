3f35e013af892c4cebb96590400bd63d50e5af24
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-05-12 01:50:07 +0900

    .

--- SingletonPattern/obj/Debug/net9.0/SingletonPattern.AssemblyInfo.cs
+++ SingletonPattern/obj/Debug/net9.0/SingletonPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39922bd8769ff0fe86fec806d26ba9f0afca180e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc810703b15d2a398c723dc5c77a2b8cee62be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
